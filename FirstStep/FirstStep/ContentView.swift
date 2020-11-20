8860d23af3fa5e86bf8eb2523f476796b7e02dc7
GiPyoo <a01054394111@gmail.com>
2020-11-21 01:11:58 +0900

    truly first commit :fire:

---
+++ FirstStep/FirstStep/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstStep
+//
+//  Created by KIM GI PYO on 2020/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstStep/FirstStep/FirstStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstStepApp.swift
+//  FirstStep
+//
+//  Created by KIM GI PYO on 2020/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
