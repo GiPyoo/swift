98edc93d7ae339b119a544c277764263f9ada9da
GiPyoo <a01054394111@gmail.com>
2020-11-21 00:26:20 +0900

    Initial Commit

---
+++ LetsStart/LetsStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsStartApp.swift
+//  LetsStart
+//
+//  Created by KIM GI PYO on 2020/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LetsStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
