a3578ca3fe8ce13bc721ec13a4d363019f971232
GiPyoo <a01054394111@gmail.com>
2020-11-23 09:55:35 +0900

    add : 클로져와 함수
    스위프트의 클로져와 함수를 공부

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -116,3 +116,143 @@ default:
     print("I don't know this color");
 }
 
+// for-in 구만 리터레이터 특성을 가진 객체 혹은 값을 순회하는 반복문
+
+let studentNames = ["kiki", "nina", "cory", "juicy"];
+var tempCount = 0;
+for studentName in studentNames {
+    if(studentName.count < 5){
+        tempCount += 3;
+        continue;
+    }
+    tempCount += 1;
+}
+
+print(tempCount); // 10
+
+
+let cases = [
+    "kiki" : 1,
+    "nina" : 3,
+    "cory" : 5
+];
+var largest = 0;
+var largestPersonName = "";
+for (name, number) in cases {
+    if(number < largest){
+        continue;
+    }
+    largest = number;
+    largestPersonName = name;
+}
+
+print(largestPersonName) // cory
+
+// while 구문.. 조건식 만큼 반복하기
+
+var n = 2;
+while n < 100 {
+    n *= 2;
+}
+print(n) // 128
+
+//repeat-while ==> do while이랑 비슷함
+
+var m = 2;
+repeat {
+    m *= 2;
+} while n < 100
+print(n) // 128
+
+
+// 항상 드는 생각이지만 do-while(repeat-while)문은 무조건 한 번은 실행하라라는 의미를 가지고 있는 것 같다.
+
+// 함수!! func
+
+func hello(person: String, day: String) -> String {
+    return "Hello, \(person), today is \(day)";
+}
+
+hello(person: "kiki", day: "Monday");
+
+// 함수 변수에 별명을 지을 수 있다. "_"는 이름없이 변수를 담게 해준다.
+func hello2(_ person: String, on day: String) -> String {
+    return "Hello, \(person), today is \(day)";
+}
+
+hello2("kiki", on: "Monday");
+
+// 튜플을 사용하여 여러개의 값을 리턴 시킬 수 있다.. 튜플은 순서가 없는 집합으로 알고 있다.
+
+let tuples = (name: "kiki", age: 26);
+
+print(tuples.name);
+print(tuples.1);
+
+//튜플 리턴 함수.. 함수 리턴 형식으로 안성맞춤인 것 같다.
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max:Int, sum:Int) {
+    var min = scores[0];
+    var max = scores[0];
+    var sum = 0;
+    
+    for score in scores {
+        if(score > max){
+            max = score;
+        }
+        if(score < min){
+            min = score;
+        }
+        sum += score;
+    }
+    
+    return (min,max,sum);
+}
+
+let statics = calculateStatistics(scores: [7,3,23,5,29,2,17,13,11]);
+print(statics.min);
+print(statics.1);
+print(statics.sum);
+
+// 바깥 함수의 변수를 참조할 수 있음 => Nested, 선언지 변수를 기억한다는 점에서 클로져?의 특성을 보임
+
+func counter() ->  (()->Int) {
+    var count = 0;
+    
+    func add() -> Int{
+        count += 1;
+        return count;
+    }
+    
+    return add;
+}
+
+let increase = counter();
+
+print(increase());
+print(increase());
+print(increase());
+print(increase());
+
+//일급객체로서의 함수
+
+func domappingInt(list: [Int], map: ((Int) -> Int)) -> [Int] {
+    var temp : [Int] = [];
+    for item in list {
+        temp.append(map(item));
+    }
+    return temp;
+}
+
+let numbers2 = [1,2,3,4,5,6];
+
+func multipletwo(number : Int) -> Int {
+    return number * 2;
+}
+
+print(domappingInt(list: numbers2, map: multipletwo));
+
+// Array의 기본함수 map
+
+let newNumbers = numbers2.map({(number:Int)->Int in number * 2});
+print(newNumbers)
